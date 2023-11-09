31d5692845359c459ae7775fc2e7304734a34b7f
wdp01015138 <wdp01015138@alibaba-inc.com>
2023-11-09 17:34:30 +0800

    组件vite版本冲突

--- app.vue
+++ app.vue
@@ -2,7 +2,7 @@
  * @Author: wdp01015138 wdp01015138@alibaba-inc.com
  * @Date: 2023-10-25 17:13:39
  * @LastEditors: wdp01015138 wdp01015138@alibaba-inc.com
- * @LastEditTime: 2023-11-09 17:32:03
+ * @LastEditTime: 2023-11-09 17:33:57
  * @FilePath: /vite/vite-vue2/app.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
@@ -13,7 +13,6 @@
   </div>
 </template>
 <script>
-import 'wdp-component'
 export default {
   components: {},
   mounted() {
